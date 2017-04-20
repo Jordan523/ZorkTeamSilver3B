102e3c1f1fe364e095bda5091f60eca7a32aa59a
jordan smith <jordan smith>
2017-04-20 17:39:22 -0400

    Class used to create enemies in the dungeon

--- Entities/Enemy.java
+++ Entities/Enemy.java
@@ -7,19 +7,20 @@ import java.util.*;
 
 import Dungeon.Dungeon;
 import Dungeon.Room;
-import Events.Die;
+import Events.*;
 import Game.GameState;
 
 public class Enemy extends Entity{
 	
 	private boolean canSeePlayer = false;
 	private boolean hasAlreadyAttackedPlayer = false;
+        private boolean alreadyAttacking = false;
 	private Room currentRoom;
 	private Item weapon;
 	private int critical;
 	private ArrayList<Item> inventory = new ArrayList<>();
 	private Random rand = new Random();
-	private AttackTimer timer = new AttackTimer(this, 5);
+	private AttackTimer timer = new AttackTimer(this, 20);
 
 	
 	public Enemy(int health, String name, Room currentRoom) {
@@ -28,14 +29,21 @@ public class Enemy extends Entity{
 		
 	}
 	
-	public Enemy(Scanner s, Dungeon d, String name) throws NoEnemyException{
+	public Enemy(Scanner s, Dungeon d, String nh) throws NoEnemyException{
 		super(s, d);
-		this.name = name;
+                
+                if(nh.equals("==="))
+			throw new NoEnemyException();
+                else if(!s.hasNextLine())
+                        throw new NoEnemyException();
+                
+		String[] nameAndHealth = nh.split(":");
+                this.name = nameAndHealth[0];
+                System.out.println(nameAndHealth[1]);
+                this.health = Integer.valueOf(nameAndHealth[1]);
 		if(!s.hasNextLine())
 			throw new NoEnemyException();
 		String next = s.nextLine();
-		if(next.equals("==="))
-			throw new NoEnemyException();
 		
 		String[] sep = next.split(":");
 		try {
@@ -48,17 +56,36 @@ public class Enemy extends Entity{
 		String[] roomSplit = next.split(":");
 		this.currentRoom = d.getRoom(roomSplit[1]);
 		d.addEnemy(this);
+                d.getRoom(currentRoom.getTitle()).addEnemy(this);
 		
 	}
 
+        
+        public void notice(Player player){
+		
+            if(!canSeePlayer){
+                System.out.println("A " + this.name + " has noticed you and is approaching!");
+                canSeePlayer = true;
+            }
+            if(!alreadyAttacking)
+                this.timer.start();
+		
+		
+	}
+        
+        
 
     public void update(){
     	
-    	if(currentRoom == GameState.instance().getAdventurersCurrentRoom()){
-    		if(!canSeePlayer)
+
+        
+        
+    	if(currentRoom == GameState.instance().getPlayer().getAdventurersCurrentRoom()){
+    		if(!canSeePlayer){
     			this.notice(GameState.instance().getPlayer());
-    		else
-    			this.attack(GameState.instance().getPlayer());
+                        return;
+                }
+                
     	}
     	else
     		canSeePlayer = false;
@@ -71,26 +98,26 @@ public class Enemy extends Entity{
 	public Item getWeapon(){
 		return this.weapon;
 	}
-	
-	public void notice(Player player){
-		
-		System.out.println("A " + this.name + " has noticed you and is approaching!");
-		canSeePlayer = true;
-		this.timer.start();
-		
-		
-	}
-    
+        
+        public Room getCurrentRoom(){
+            return this.currentRoom;
+            
+        }
+        
+        public void setCurrentRoom(Room room){
+            this.currentRoom = room;
+        }
+	 
 
 	
 	public void attack(Player player){
 		
 		int waitChance = rand.nextInt(100) + 1;
-		boolean firstBlood = false;
+		this.alreadyAttacking = true;
 		
 		if(canSeePlayer && !hasAlreadyAttackedPlayer){
 				
-				System.out.println("The " + this.name + " approached you!");
+				System.out.println("\nThe " + this.name + " approached you!");
 				this.hasAlreadyAttackedPlayer = true;
 		
 		}
@@ -104,13 +131,15 @@ public class Enemy extends Entity{
 		
 		case 1:
 			damageDone = this.getDamage();
-			System.out.println("The " + this.getName() + " attacked you!");
+			System.out.println("\nThe " + this.name + " attacked you!");
 			player.reduceHealth(damageDone);
+                        this.timer.reset();
 			break;
 		case 2:
 			damageDone = this.getDamage();
-			System.out.println("The " + this.getName() + " attacked you with it's " + this.weapon.getPrimaryName() +"!");
+			System.out.println("\nThe " + this.name + " attacked you with it's " + this.weapon.getPrimaryName() +"!");
 			player.reduceHealth(damageDone);
+                        this.timer.reset();
 			break;
 		}
 		
@@ -118,9 +147,16 @@ public class Enemy extends Entity{
 			player.setHealth(0);
 		}
 		
-		this.timer.start();
 	}
 	
+        public boolean canSeePlayer(){
+            return this.canSeePlayer;
+        }
+        
+        public void setCanSeePlayer(boolean x){
+            this.canSeePlayer = x;
+        }
+        
 	public int getHealth(){
 		return this.health;
 	}
@@ -129,16 +165,36 @@ public class Enemy extends Entity{
 		this.health = health;
 	}
 	
-	public void reduceHealth(int healthToReduce){
+	public void reduceHealth(int healthToReduce) throws NoItemException{
 		
 		System.out.println(this.name + " took " + healthToReduce + " damage!");
 		
 		
 		this.health -= healthToReduce;
+                
+               if(this.health <= 0){
+                    int potionChance = rand.nextInt(100) + 1;
+                    System.out.println("The " + this.name + " was defeated! You have gained 20 points!");
+                    GameState.instance().getPlayer().addScore(20);
+                    this.timer.stop();
+                    this.currentRoom.addDefeatedEnemy(this);
+                    this.currentRoom.removeEnemy(this.name);
+                    if(potionChance > 0){
+                        System.out.println("The " + this.name + " dropped a potion!");
+                        GameState.instance().getPlayer().getAdventurersCurrentRoom().add(new Item(GameState.instance().getDungeon().getItem("Potion")));
+                    }
+                    
+                }
 		
 		
 		
 	}
+        
+        public void resetTimer(){
+            this.timer.reset();
+        }
+        
+        
 	
 	public static class NoEnemyException extends Exception{}
 }
