e8e3098da8aacfe171100ea860507b2328a4980e
J523 <jordan52397@aol.com>
2017-04-24 15:55:05 -0400

    May or may not use. Depends on time.

--- Events/UseEvent.java
+++ Events/UseEvent.java
@@ -1,13 +1,27 @@
 package Events;
 
+import Entities.Player;
+import Game.GameState;
 import Items.Item;
+import Items.Item.NoItemException;
 
 public class UseEvent implements Events{
 	
 	private Item theItem;
+	private String newItem;
+	private Player player = GameState.instance().getPlayer();
 
 	public UseEvent(Item item){
 		this.theItem = item;
+	}
+	
+	public UseEvent(String item, String newItem){
+		try {
+			theItem = GameState.instance().getDungeon().getItem(item);
+			this.newItem = newItem;
+		} catch (NoItemException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -31,6 +45,15 @@ public class UseEvent implements Events{
 		//}
 		//else
 			//new DisappearEvent(this.theItem).execute();
+		
+		for(Item x : player.getInventory()){
+			if(x.equals(theItem)){
+				
+				new TransformEvent(newItem, theItem);
+				return;
+			}
+		}
+		
 		
 		
 		
