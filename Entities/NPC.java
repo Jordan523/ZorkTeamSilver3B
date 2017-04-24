a1f86ef7a1e62238813e0853a4bdb7e02ed411c3
J523 <jordan52397@aol.com>
2017-04-24 19:50:17 -0400

    Added ability to associate events with items

--- Entities/NPC.java
+++ Entities/NPC.java
@@ -1,13 +1,19 @@
 package Entities;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.Random;
 import java.util.Scanner;
 
 import Dungeon.Dungeon;
 import Dungeon.Room;
 import Entities.Enemy.NoEnemyException;
+import Entities.Player.TooHeavyException;
+import Events.Events;
+import Events.Win;
 import Game.GameState;
+import Items.Item;
+import Items.Item.NoItemException;
 
 public class NPC extends Entity{
 
@@ -15,6 +21,8 @@ public class NPC extends Entity{
 	private Scanner s;
 	private Room currentRoom;
 	private String idle;
+	private ArrayList<Item> inventory = new ArrayList<Item>();
+	private Hashtable<Item, Events> ite = new Hashtable<>();
 	
 	public NPC(String name,Scanner s, Dungeon d) throws NoEnemyException{
 		super(name);
@@ -59,6 +67,19 @@ public class NPC extends Entity{
 						System.out.println(message.replace("-", ""));
 						this.messages.add(message.replace("-", ""));
 						message = s.nextLine();
+						if(message.equalsIgnoreCase("Events:")){
+							String nextEvent = s.nextLine();
+							while(!nextEvent.equals("---")){
+								String[] sep = nextEvent.split(":");
+								if(sep[0].equalsIgnoreCase("Win")){
+									try {
+										this.ite.put(d.getItem(sep[1]), new Win());
+									} catch (NoItemException e) {}
+									nextEvent = s.nextLine();
+								}
+								message = nextEvent;
+							}
+						}
 						
 					}
 				}
@@ -87,12 +108,44 @@ public class NPC extends Entity{
 		
 	}
 	
+	public void update(){
+		
+		try{
+			for(Item x : inventory){
+				
+				this.ite.get(x).execute();
+				
+			}
+		}catch(Exception e){}
+		
+	}
+	
+	public void giveItem(Item item){
+		this.inventory.add(item);
+		this.update();
+	}
+	
+	public void takeItem(Item item){
+		try {
+			GameState.instance().getPlayer().addToInventory(item);
+			inventory.remove(item);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			System.out.println("I don't know how to do that.");
+		}
+		
+	}
+	
 	public Room getCurrentRoom(){
 		return this.currentRoom;
 	}
 	
 	public String getIdleMessage(){
 		return this.idle;
+	}
+	
+	public ArrayList<Item> getInventory(){
+		return inventory;
 	}
 }
 
