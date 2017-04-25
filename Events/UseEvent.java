2b31d1ecf7823fc114b5800e30fc7ad6de1ce59d
J523 <jordan52397@aol.com>
2017-04-24 22:11:14 -0400

    Items can now be used to transform other items.

--- Events/UseEvent.java
+++ Events/UseEvent.java
@@ -8,20 +8,17 @@ import Items.Item.NoItemException;
 public class UseEvent implements Events{
 	
 	private Item theItem;
-	private String newItem;
+	private String itemString;
 	private Player player = GameState.instance().getPlayer();
-
+	
 	public UseEvent(Item item){
 		this.theItem = item;
 	}
 	
-	public UseEvent(String item, String newItem){
-		try {
-			theItem = GameState.instance().getDungeon().getItem(item);
-			this.newItem = newItem;
-		} catch (NoItemException e) {
-			e.printStackTrace();
-		}
+	public UseEvent(String itemString){
+		
+		this.itemString = itemString;
+		
 	}
 
 	@Override
@@ -46,15 +43,26 @@ public class UseEvent implements Events{
 		//else
 			//new DisappearEvent(this.theItem).execute();
 		
+		String[] sep = itemString.split("-");
+		String item = sep[0];
+		String newItem = sep[1];
+		
+		try {
+			theItem = GameState.instance().getDungeon().getItem(item);
+		} catch (NoItemException e) {
+			System.out.println("What?");
+		}
+		
 		for(Item x : player.getInventory()){
 			if(x.equals(theItem)){
 				
-				new TransformEvent(newItem, theItem);
+				new TransformEvent(newItem, theItem).execute();
+				System.out.println(item + " was changed to a " + newItem);
 				return;
 			}
 		}
 		
-		
+		System.out.println("You don't have the necessary item to use this.");
 		
 		
 	}
